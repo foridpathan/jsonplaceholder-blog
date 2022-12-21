486a365739983af609cdfb8fa8ac41e1a8e6b98d
foridpathan <foridpathan45@gmail.com>
2022-12-21 05:25:00 +0000

    Deploying to public from @ foridpathan/jsonplaceholder-blog@5f96bff58b2b091492273613c3b1d28acd6efb32 🚀

---
+++ _next/static/1s-y_4NeQSEfjQ7OKWaIl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-572fe3f281d05bb0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/post/[id]":["static/chunks/pages/post/[id]-1468c5c260f3c1ea.js"],sortedPages:["/","/_app","/_error","/post/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
